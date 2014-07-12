9fa26d3bd36a08b8ab3615c963732668e754191a
Ivo Georgiev <ivo@linvo.org>
2014-07-12 18:30:25 +0300

    Implemented FS persistence, 0.0.2

--- index.js
+++ index.js
@@ -1,11 +1,17 @@
+var fs = require("fs");
+
 var Container = typeof Buffer !== "undefined" ? Buffer //in node, use buffers
 		: typeof Int8Array !== "undefined" ? Int8Array //in newer browsers, use webgl int8arrays
 		: function(l){ var a=new Array(l); for(var i=0;i<l;i++)a[i]=0; }; //else, do something similar
 
-function BitField(data){
+function BitField(data, persist){
 	if(!(this instanceof BitField)) {
-		return new BitField(data);
+		return new BitField(data, persist);
 	}
+
+	this.persist = persist;
+	if (persist) try { this.buffer = fs.readFileSync(persist); return; } catch(e) { };
+
 	if(typeof data === "number"){
 		if(data % 8 !== 0) data += 1 << 3;
 		data = new Container(data >> 3);
@@ -24,6 +30,10 @@ BitField.prototype.set = function(i, b){
 	} else {
 		this.buffer[i >> 3] &= ~(128 >> (i % 8));
 	}
+
+	if (this.persist) fs.writeFile(this.persist, this.buffer, function(err) { 
+		if (err) console.error(err) 
+	});
 };
 
 if(typeof module !== "undefined") module.exports = BitField;
